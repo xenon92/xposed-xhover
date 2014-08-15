6bfddff74d57a871354158ae50c4a1e54414de03
Shubhang Rathore <shubhang92@gmail.com>
2014-08-15 15:15:53 +0530

    Enable "Reset all" preference to restore stock Paranoid Android values

--- app/src/main/java/com/shubhangrathore/xposed/xhover/MainActivity.java
+++ app/src/main/java/com/shubhangrathore/xposed/xhover/MainActivity.java
@@ -1,14 +1,15 @@
 package com.shubhangrathore.xposed.xhover;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
@@ -18,10 +19,12 @@ public class MainActivity extends PreferenceActivity implements Preference.OnPre
     private static final String PREF_LONG_FADE_OUT_DELAY = "long_fade_out_delay";
     private static final String PREF_SHORT_FADE_OUT_DELAY = "short_fade_out_delay";
     private static final String PREF_MICRO_FADE_OUT_DELAY = "micro_fade_out_delay";
+    private static final String PREF_RESET_ALL = "reset_all";
 
-    private Preference mLongFadeOutDelay;
-    private Preference mShortFadeOutDelay;
-    private Preference mMicroFadeOutDelay;
+    private ListPreference mLongFadeOutDelay;             // Natural timeout preference
+    private ListPreference mShortFadeOutDelay;            // Notification waiting preference
+    private ListPreference mMicroFadeOutDelay;            // Evade notification preference
+    private Preference mResetAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +46,16 @@ public class MainActivity extends PreferenceActivity implements Preference.OnPre
         // ########################################################## //
 
 
-        mMicroFadeOutDelay = findPreference(PREF_MICRO_FADE_OUT_DELAY);
+        mMicroFadeOutDelay = (ListPreference) findPreference(PREF_MICRO_FADE_OUT_DELAY);
         mMicroFadeOutDelay.setOnPreferenceChangeListener(this);
 
-        mShortFadeOutDelay = findPreference(PREF_SHORT_FADE_OUT_DELAY);
+        mShortFadeOutDelay = (ListPreference) findPreference(PREF_SHORT_FADE_OUT_DELAY);
         mShortFadeOutDelay.setOnPreferenceChangeListener(this);
 
-        mLongFadeOutDelay = findPreference(PREF_LONG_FADE_OUT_DELAY);
+        mLongFadeOutDelay = (ListPreference) findPreference(PREF_LONG_FADE_OUT_DELAY);
         mLongFadeOutDelay.setOnPreferenceChangeListener(this);
+
+        mResetAll = findPreference(PREF_RESET_ALL);
     }
 
 
@@ -66,17 +71,39 @@ public class MainActivity extends PreferenceActivity implements Preference.OnPre
     }
 
     @Override
-    public boolean onPreferenceTreeClick (PreferenceScreen preferenceScreen, Preference preference) {
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+
+        if (preference == mResetAll) {
+            resetConfirmation();
+        }
         return false;
     }
 
     /**
      * Confirms the action from user before resetting all the
-     * values to stock values as defined by ParanoidAndroid.
-     *
-     * @return boolean true if user confirms reset, else false
+     * values to stock values as defined by Paranoid Android.
      */
-    private boolean resetConfirmation() {
-        return false;
+    private void resetConfirmation() {
+        new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.reset_all))
+                .setMessage(getString(R.string.reset_all_confirmation_message))
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        resetAllPreferences();
+                        Toast.makeText(getApplicationContext(), getString(R.string.stock_values_restored), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Intentionally left blank
+                    }
+                })
+                .show();
+    }
+
+    private void resetAllPreferences() {
+        mLongFadeOutDelay.setValue("5000");
+        mShortFadeOutDelay.setValue("2500");
+        mMicroFadeOutDelay.setValue("1250");
     }
 }
