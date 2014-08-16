4129c964a35c207391c58cf87d3831befbea40e2
Shubhang Rathore <shubhang92@gmail.com>
2014-08-16 19:14:51 +0530

    Implement custom "Notification waiting" timeout
    "Notification waiting" timeout is the duration for which the
    current Hover notification stays on screen if there is another
    notification waiting to be shown in Hover.
    
    If the user has expanded the current notification, this timeout
    duration is overwritten by "Natural timeout" duration.
    
    Signed-off-by: Shubhang Rathore <shubhang92@gmail.com>

--- app/src/main/java/com/shubhangrathore/xposed/xhover/XposedHover.java
+++ app/src/main/java/com/shubhangrathore/xposed/xhover/XposedHover.java
@@ -17,6 +17,7 @@ public class XposedHover implements IXposedHookLoadPackage {
     private static final String PACKAGE_SYSTEM_UI = "com.android.systemui";
 
     private static String sCallingMethod = "";
+    private static boolean sExpanded;
 
     private int mMicroFadeOutDelay;            // Evade notification time delay
     private int mShortFadeOutDelay;            // Notification waiting time delay
@@ -151,14 +152,80 @@ public class XposedHover implements IXposedHookLoadPackage {
 
                 } else if (sCallingMethod.equals("startLongHideCountdown")) {
 
-                // Detected that startLongHideCountdown() method called
-                // startHideCountdown(int) method.
-                // Overriding 'Natural timeout' values here.
-                XposedBridge.log("Setting startHideCountdown delay to: "
-                        + String.valueOf(mLongFadeOutDelay));
+                    // Detected that startLongHideCountdown() method called
+                    // startHideCountdown(int) method.
+                    // Overriding 'Natural timeout' values here.
+                    XposedBridge.log("Setting startHideCountdown delay to: "
+                            + String.valueOf(mLongFadeOutDelay));
 
-                methodHookParam.args[0] = mLongFadeOutDelay;
+                    methodHookParam.args[0] = mLongFadeOutDelay;
+                }
             }
+        });
+
+
+        // Hooking to method processOverridingQueue(boolean) that processes notifications
+        // in queue and calls startOverrideCountdown(int) based on two things:
+        // 1. If there are multiple notifications to waiting in queue to be shown
+        // 2. If the current notification is expanded
+        //
+        // (1 && 2) -> mLongFadeOutDelay
+        // (1 && !2) -> mShortFadeOutDelay
+        // (!1 && 2) -> mLongFadeOutDelay
+        // (!1 && !2) -> mShortFadeOutDelay
+        XposedHelpers.findAndHookMethod(mHoverClass, "processOverridingQueue", boolean.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+
+                XposedBridge.log("Calling method = processOverridingQueue()");
+                sCallingMethod = "processOverridingQueue";
+
+                // sExpanded stores the value of the boolean argument "expanded" which is passed
+                // to processOverridingQueue(boolean). This denoted if the current notification
+                // view being processed is expanded notification or not.
+                sExpanded = Boolean.parseBoolean(String.valueOf(methodHookParam.args[0]));
+                XposedBridge.log("Notification is expanded before " +
+                        "processOverridingQueue() call = " + sExpanded);
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+
+                // Resetting flags after processOverridingQueue method has executed
+                // completely so as to prevent overriding wrong sCallingMethod detection
+                // and wrong notification expanded state
+                sCallingMethod = "";
+                sExpanded = false;
+            }
+        });
+
+
+        // This uses the time delay value sent by processOverridingQueue(boolean)
+        // capturing the delay in int argument and using the delay to set
+        // duration for current Hover notification.
+        XposedHelpers.findAndHookMethod(mHoverClass, "startOverrideCountdown", int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+
+                if (sCallingMethod.equals("processOverridingQueue")) {
+                    if (sExpanded) {
+
+                        // If here, it means that there are more than one notifications
+                        // in queue and the notification currently being shown is expanded.
+                        // Therefore, using mLongFadeOutDelay for current notification in Hover.
+                        XposedBridge.log("startOverrideCountdown delay override " +
+                                "- EXPANDED = " + mLongFadeOutDelay);
+                        methodHookParam.args[0] = mLongFadeOutDelay;
+                    } else {
+
+                        //If here, it means that there are more than one notifications
+                        // in queue and the notification currently being shown is not expanded.
+                        // Therefore, using mShortFadeOutDelay for current notification in Hover.
+                        XposedBridge.log("startOverrideCountdown delay override " +
+                                "- NOT EXPANDED = " + mShortFadeOutDelay);
+                        methodHookParam.args[0] = mShortFadeOutDelay;
+                    }
+                }
             }
         });
     }
